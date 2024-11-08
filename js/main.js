4b9b62c3f4b1a45721ddef0bb1db313b0b6de734
ahmedmuti <ahmedmutti@gmail.com>
2024-11-08 03:03:08 +0200

    loading

--- js/main.js
+++ js/main.js
@@ -4,10 +4,12 @@ document.addEventListener("DOMContentLoaded", function () {
     function checkScroll() {
         // Get the position of the second section
         const secondSectionTop = secondSection.getBoundingClientRect().top;
+        const navbarSectionTop = navbar.getBoundingClientRect().top;
+
 
         // Change the background of the navbar when the second section is reached
 
-        if (secondSectionTop <= 250) {
+        if (window.scrollY > 50) {
 
             navbar.classList.add("scrolled");
         } else {
@@ -23,16 +25,17 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener("DOMContentLoaded", function () {
     const toggler = document.querySelector(".navbar-toggler");
     const icon = toggler.querySelector(".toggler-icon");
+    const navbar = document.querySelector(".navbar .container");
 
     toggler.addEventListener("click", function () {
         icon.classList.toggle("open");
         if (icon.classList.contains("open")) {
             icon.classList.replace("ri-menu-2-line", "ri-close-line");
-            navbar.classList.add("main-bg");
+            navbar.classList.add("main-bg", 'pt-2', 'rounded-3');
 
         } else {
             icon.classList.replace("ri-close-line", "ri-menu-2-line");
-            navbar.classList.remove("main-bg");
+            navbar.classList.remove("main-bg", 'pt-2', 'rounded-3');
 
         }
     });
@@ -80,9 +83,20 @@ document.addEventListener("DOMContentLoaded", function () {
     const footerHeight = footer.clientHeight;
 
     // Set the height of the target section
-    targetSection.style.marginBottom = `${footerHeight}px`;
+    targetSection.style.marginBottom = `${footerHeight + 60}px`;
 });
 
 
 AOS.init();
 AOS.refresh();
+
+window.addEventListener("load", function () {
+    const loadingScreen = document.getElementById("loading");
+    const mainContent = document.getElementById("main");
+    setTimeout(() => {
+        loadingScreen.style.display = "none"; // Hide loading screen
+        document.body.style.overflow = "auto"; // Hide loading screen
+    }, 3000)
+
+    mainContent.style.display = "block";  // Show main content
+});
